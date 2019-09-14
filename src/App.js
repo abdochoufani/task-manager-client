8fbee561011e5bc7717c42f6557d39a9e3b8374b
Abdo Choufani <abdo.elchoufani@gmail.com>
2019-09-14 20:41:48 +0300

    connect user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavTop from './components/nav/NavTop'
+import NavBottom from './components/nav/NavBottom'
+import Modal from './components/auth/Modal'
+import TaskList from './components/tasks/TaskList'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component  {
+  state={
+    socket:null,
+    status:'disconected',
+    error:'',
+    show:true,
+    tasks:[]
+  }
+
+
+  handleClose = () => {
+    this.setState({show:false})
+  }
+
+  handleStatus = (status) => {
+    this.setState({status})
+  }
+
+  handleTasks = (tasks) => {
+    this.setState({tasks})
+  }
+  render(){
+    const {show, tasks} =this.state
+    return (
+      <div className="App">
+        <NavTop />
+        <NavBottom />
+        <Modal
+          handleStatus={this.handleStatus}
+          handleClose = {this.handleClose} 
+          status={this.state.status} 
+          show={this.state.show}
+          />
+        <TaskList/>
+        { (show===false && tasks===[]) && (
+          <div>
+            <h1>HHHHHHHHH</h1>hello
+          </div>
+        )}
+      </div>
+    )}
 }
 
 export default App;
