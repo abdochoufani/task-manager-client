559d2cc60925eef2a448e52f093966c4f5fc4792
Abdo Choufani <abdo.elchoufani@gmail.com>
2019-09-16 20:44:45 +0300

    handeling errors

--- src/components/tasks/TaskForm.js
+++ src/components/tasks/TaskForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import encrypt from '../../utils/Encryption'
 
 export default class TaskForm extends Component {
     state= {
@@ -17,13 +18,13 @@ export default class TaskForm extends Component {
     }
     handleSubmit =(event) => {
         event.preventDefault()
-        this.setState({i : this.state.i + 1})
         this.props.handleTaskForm()
         const tasks = this.props.tasks
-        tasks.push(this.state.task)
-        debugger
-        const json =JSON.stringify(tasks)
-        this.props.socket.emit('setTasks', {username:sessionStorage.getItem('username'),  data:json})
+        tasks.push({task:this.state.task, completed:false})
+        let json =JSON.stringify(tasks)
+        const encrypted = encrypt.encryptData(json, sessionStorage.getItem('hash'))
+        this.props.socket.emit('setTasks', {username:sessionStorage.getItem('username'),  data:encrypted})
+        this.props.socket.emit('getTasks')
     }
     render() {
         
